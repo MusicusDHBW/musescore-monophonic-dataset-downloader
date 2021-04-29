6c1c622daea59578d14453ca803d65277509dde7
Markus Neugebauer <mail@markusneugebauer.de>
2021-04-29 20:05:05 +0200

    adding error handling to classification batch run

--- extract_batch.py
+++ extract_batch.py
@@ -2,12 +2,20 @@ import os
 
 import extract
 
-directory = 'out_dev'
+directory = 'out_eval_coco'
 files = [file.replace('.musicxml', '') for file in os.listdir(directory) if file.endswith('.musicxml')]
 
 for file in files:
     score = extract.Score()
     print(file)
-    classification = score.classify(f'{directory}/{file}-1.svg', f'{directory}/{file}.musicxml')
-    with open(f'{directory}/{file}.coord', 'w') as f:
+    try:
+        classification = score.classify(f'{directory}/{file}.svg', f'{directory}/{file}.musicxml')
+    except:
+        os.remove(f'{directory}/{file}.musicxml')
+        os.remove(f'{directory}/{file}.svg')
+        os.remove(f'{directory}/{file}.png')
+        with open(f'{directory}/error.log', 'a') as f:
+            f.write(f'{file} classify\n')
+        continue
+    with open(f'{directory}/{file}.coco', 'w') as f:
         f.write(classification)
