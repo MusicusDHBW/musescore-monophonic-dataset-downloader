7110fb1f6d66928897f28da4547ee74d7d9d9e00
Markus Neugebauer <mail@markusneugebauer.de>
2021-05-23 14:41:49 +0200

    deleting comments

--- download.py
+++ download.py
@@ -2,10 +2,7 @@ import sys
 import requests
 import subprocess
 import os
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     files = open(sys.argv[1], 'r').read().split('\n')
     outputDir = sys.argv[2]
@@ -19,17 +16,3 @@ if __name__ == '__main__':
         request = requests.get(url, allow_redirects=True)
         with open(outputDir + '/' + ident + '.mscz', 'wb') as out:
             out.write(request.content)
-
-        #path = os.getcwd() + "\\" + outputDir + "\\" + ident
-        #print(path)
-        #png = path + ".png"
-        #print(png)
-        #mscz = path + ".mscz"
-        #print(mscz)
-        #command = ["MuseScore3", "-o", png, mscz]
-        #print(command)
-        #subprocess.run(command)
-        #subprocess.run(["MuseScore3", "-o", path + "/" + ident + ".musicxml", path + "/" + ident + ".mscz"])
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- search.py
+++ search.py
@@ -1,10 +1,6 @@
-# This is a sample Python script.
 import sys
 import re
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     fileRefs = open(sys.argv[2], "r").read()
     fileIds = open(sys.argv[1], "r").read().split('\n')
@@ -15,6 +11,3 @@ if __name__ == '__main__':
         if ref:
             with open(sys.argv[3], "a") as f:
                 f.write(ident + ':' + ref.group(0) + "\n")
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
